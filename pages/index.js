a2a166bc2eb22df070d369777d342d5c3b0221de
devinlinux <ssmith969898@gmail.com>
2024-07-21 18:57:28 -0400

    Added dates

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,59 @@
-import { Container, Box, Heading, Center, Text } from '@chakra-ui/react'
+import {
+    Container,
+    Box,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    useColorModeValue,
+} from '@chakra-ui/react'
 import Banner from '../components/banner'
+import Layout from '../components/layouts/article'
 
 const Page = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
-        <Banner />
+        <Layout>
+            <Banner />
+
+            <Container>
+                <Box
+                    borderRadius="lg"
+                    mb={6}
+                    p={3}
+                    textAlign="center"
+                    bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                >
+                    <b>January 18-19, 2025</b>
+
+                    <br></br>
+                    <Button onClick={onOpen}>Rain Date</Button>
+
+                    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                        <ModalOverlay />
+                        <ModalContent>
+                            <ModalHeader>HackBI Rain Date</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                In case of any inclement weather, HackBI will occur on <b>January 25-26, 2025</b>.
+                            </ModalBody>
+
+                            <ModalFooter>
+                                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                    Close
+                                </Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
+                </Box>
+            </Container>
+        </Layout>
     )
 }
 
