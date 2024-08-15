d2db705d99886db2ab07a6ee2a3e849b1ae9eb38
devinlinux <ssmith969898@gmail.com>
2024-08-14 23:36:46 -0400

    Added circuits page

---
+++ pages/workshops/circuits.js
@@ -0,0 +1,31 @@
+import {
+    Container,
+    Badge,
+} from '@chakra-ui/react'
+import Layout from "../../components/layouts/article";
+import { Title, WorkImage, Meta } from '../../components/workshop'
+import Paragraph from '../../components/paragraph';
+
+const EngineeringCircuits = () => {
+    return (
+        <Layout title="Engineering Circuits">
+            <Container>
+                <Title>
+                    Engineering Circuits <Badge>Daniel Santillian | Nick Danshaw | Dallas DSouza</Badge>
+                </Title>
+
+                <Paragraph>
+                    Engineering Circuits is a fascinating field that explores the fundamental principles of electrical and electronic circuits. This
+                    area of study involves designing, analyzing, and constructing circuits to understand how electrical systems operate and interact.
+                    From basic components like resistors and capacitors to complex integrated circuits, learning about circuits provides essential
+                    knowledge for developing a wide range of technologies. Whether you're working on creating innovative electronic devices or
+                    solving real-world engineering problems, mastering circuit design and analysis is crucial. With hands-on experience in circuit
+                    building and simulation tools, you'll gain valuable skills that are applicable in various industries, from consumer
+                    electronics to industrial automation.
+                </Paragraph>
+            </Container>
+        </Layout>
+    )
+}
+
+export default EngineeringCircuits;
