34cfab094ecc3f8d951fbc109645e177b9c4611d
devinlinux <ssmith969898@gmail.com>
2024-08-16 17:12:54 -0400

    Added contact page

--- pages/contact.js
+++ pages/contact.js
@@ -5,32 +5,22 @@ import {
     FormControl,
     FormLabel,
     Input,
-    Heading,
+    Link,
     Text,
     VStack,
-    Modal,
-    ModalBody,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    ModalFooter,
     useToast,
-    useDisclosure,
-    useColorModeValue,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import Layout from '../components/layouts/article';
 import AnimatedBox from "../components/animated-box";
-import Paragraph from "../components/paragraph";
 require('dotenv').config()
 
-const PreRegister = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+const Contact = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [gradeLevel, setGradeLevel] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
     const toast = useToast();
 
     const handleSubmit = async (e) => {
@@ -40,16 +30,18 @@ const PreRegister = () => {
         const apiKey = process.env.NEXT_PUBLIC_PICKET_API_KEY
 
         const payload = {
+            api_key: apiKey,
             name,
             email,
-            grade_level: parseInt(gradeLevel, 10)
+            subject,
+            message,
         };
 
         try {
-            const response = await fetch(`${apiURL}/pre-register`, {
+            const response = await fetch(`${apiURL}/`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(payload)
             });
@@ -77,50 +69,9 @@ const PreRegister = () => {
     };
 
     return (
-        <Layout title="Pre-Register">
+        <Layout title="Contact">
             <Container>
                 <AnimatedBox delay={0.1}>
-                    <Box
-                        borderRadius="lg"
-                        mb={6}
-                        p={3}
-                        textAlign="center"
-                        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-                    >
-                        <Box pb='1'>
-                            <Text as='b'>January 18-19, 2025</Text>
-                        </Box>
-
-                        <Button onClick={onOpen} colorScheme='cyan'>Rain Date</Button>
-
-                        <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                            <ModalOverlay />
-                            <ModalContent>
-                                <ModalHeader>HackBI Rain Date</ModalHeader>
-                                <ModalCloseButton />
-                                <ModalBody>
-                                    In case of any inclement weather, HackBI will occur on <b>January 25-26, 2025</b>.
-                                </ModalBody>
-
-                                <ModalFooter>
-                                    <Button colorScheme='blue' mr={3} onClick={onClose}>
-                                        Close
-                                    </Button>
-                                </ModalFooter>
-                            </ModalContent>
-                        </Modal>
-                    </Box>
-                </AnimatedBox>
-
-                <Box pb={4}>
-                    <Heading as="h3" variant="section-title">Get Notified</Heading>
-                    <Paragraph>
-                        Enter your name, email address, and current grade level to be added to
-                        our email list to be ontified when registration is open
-                    </Paragraph>
-                </Box>
-
-                <AnimatedBox delay={0.15}>
                     <Box maxW="md" mx="auto" pb={6}>
                         <form onSubmit={handleSubmit}>
                             <VStack spacing={4}>
@@ -141,16 +92,27 @@ const PreRegister = () => {
                                     />
                                 </FormControl>
                                 <FormControl isRequired>
-                                    <FormLabel>Grade Level</FormLabel>
+                                    <FormLabel>Subject</FormLabel>
                                     <Input
-                                        type="number"
-                                        value={gradeLevel}
-                                        onChange={(e) => setGradeLevel(e.target.value)}
+                                        type="text"
+                                        value={subject}
+                                        onChange={(e) => setSubject(e.target.value)}
+                                    />
+                                </FormControl>
+                                <FormControl isRequired>
+                                    <FormLabel>Message</FormLabel>
+                                    <Input
+                                        type="text"
+                                        value={message}
+                                        onChange={(e) => setMessage(e.target.value)}
                                     />
                                 </FormControl>
                                 <Button type="submit" colorScheme="cyan" width="full">
                                     Submit
                                 </Button>
+                                <Text fontSize="sm" color="gray.500" align="center">
+                                    You can also email us at <Link href="mailto:hackbi@bishopireton.org">hackbi@bishopireton.org</Link>
+                                </Text>
                             </VStack>
                         </form>
                     </Box>
@@ -160,4 +122,4 @@ const PreRegister = () => {
     )
 }
 
-export default PreRegister
+export default Contact
