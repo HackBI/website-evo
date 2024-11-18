56e35b0e22567535744ce5306637508749a07998
devinlinux <ssmith969898@gmail.com>
2024-11-18 09:52:38 -0500

    Changed contact functionality

--- pages/contact.js
+++ pages/contact.js
@@ -13,11 +13,8 @@ import {
 import React, { useState } from 'react';
 import Layout from '../components/layouts/article';
 import AnimatedBox from "../components/animated-box";
-import CryptoJS from 'crypto-js';
-require('dotenv').config()
 
 const Contact = () => {
-
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
@@ -25,86 +22,12 @@ const Contact = () => {
     const [message, setMessage] = useState('');
     const toast = useToast();
 
-    const computeSecret = (apiKey, hashKey, timestamp, iterations, firstSubIndex, secondSubIndex, splitIndex) => {
-        const time1 = timestamp.substring(0, splitIndex);
-        const time2 = timestamp.substring(splitIndex);
-
-        let secret = apiKey.substring(0, firstSubIndex) + time1 + apiKey.substring(firstSubIndex, secondSubIndex) + time2 + apiKey.substring(secondSubIndex);
-
-        let secretBytes = CryptoJS.enc.Utf8.parse(secret);
-
-        for (let i = 0; i < iterations; i++) {
-            let hmac = CryptoJS.HmacSHA256(secretBytes, hashKey);
-            secretBytes = hmac;
-        }
-
-        return secretBytes.toString(CryptoJS.enc.Hex);
-    };
-
-    const handleSubmit = async (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        const apiURL = String(process.env.NEXT_PUBLIC_PICKET_API_URL);
-        const apiKey = String(process.env.NEXT_PUBLIC_PICKET_API_KEY);
-        const hashKey = String(process.env.NEXT_PUBLIC_PICKET_HASH_KEY);
-        const timestamp = Math.floor(Date.now() / 1000).toString();
-        const iterations = String(process.env.NEXT_PUBLIC_PICKET_HASH_ITERATIONS);
-        const firstSubIndex = String(process.env.NEXT_PUBLIC_PICKET_FIRST_SUB_INDEX);
-        const secondSubIndex = String(process.env.NEXT_PUBLIC_PICKET_SECOND_SUB_INDEX);
-        const splitIndex = String(process.env.NEXT_PUBLIC_PICKET_SPLIT_INDEX);
+        const mailtoLink = `mailto:hackbi@bishopireton.org?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(`Name: ${firstName} ${lastName}\nEmail: ${email}\n\nMessage: ${message}`)}`;
 
-        console.log("URL1: " + process.env.NEXT_PUBLIC_PICKET_API_KEY);
-
-        const secret = computeSecret(
-            apiKey,
-            hashKey,
-            timestamp,
-            parseInt(iterations, 10),
-            parseInt(firstSubIndex, 10),
-            parseInt(secondSubIndex, 10),
-            parseInt(splitIndex, 10)
-        );
-
-        const payload = {
-            first_name: firstName,
-            last_name: lastName,
-            email,
-            subject,
-            message,
-        };
-
-        try {
-            const response = await fetch(`${apiURL}/contact`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-request-timestamp': timestamp,
-                    'x-request-secret': secret,
-                },
-                body: JSON.stringify(payload)
-            });
-
-            if (!response.ok) {
-                const errorMsg = await response.text()
-                throw new Error(`${errorMsg}`);
-            }
-
-            toast({
-                title: "Success!",
-                description: "Your message was submitted.",
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-            });
-        } catch (error) {
-            toast({
-                title: "An error occurred while processing:",
-                description: error.message,
-                status: "error",
-                duration: 5000,
-                isClosable: true,
-            });
-        }
+        window.location.href = mailtoLink;
     };
 
     return (
@@ -166,7 +89,7 @@ const Contact = () => {
                 </AnimatedBox>
             </Container>
         </Layout>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
