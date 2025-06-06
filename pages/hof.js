50072103958c39a7820c5f3dcc12e56c0094806b
devinlinux <ssmith969898@gmail.com>
2025-06-06 13:20:25 -0400

    golden banner use

--- pages/hof.js
+++ pages/hof.js
@@ -6,39 +6,21 @@ import {
     Text,
 } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
+import GoldenBanner from '../components/golden-banner'
 import AnimatedBox from '../components/animated-box'
 import Liquid from '../components/liquid'
 import SpecialText from '../components/SpecialText'
 
 const HOF = () => {
     return (
+        <Layout title="Hall of Fame">
+            <GoldenBanner />
             <Container maxW="container.lg">
                 <Box>
-                    <Liquid
-                        baseColor={[0.38, 0.32, 0.1]}
-                        speed={0.3}
-                        amplitude={0.51}
-                        interactive={false}
-                    >
-                        <Box
-                            m={6}
-                            p={2}
-                            borderRadius="md"
-                        >
-                            <Center>
-                                <SpecialText speed={3}>
-                                    <Text
-                                        fontSize="7xl"
-                                        fontWeight="bold"
-                                    >
-                                        HackBI{'\n'}HALL OF FAME
-                                    </Text>
-                                </SpecialText>
-                            </Center>
-                        </Box>
-                    </Liquid>
+                    
                 </Box>
             </Container>
+        </Layout>
     )
 }
 
